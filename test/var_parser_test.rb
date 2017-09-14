4d8afa1ea2a7403e3ceb103978c19a8859c671fd
dinesh.theetla@gmail.com <dinesh>
2017-09-14 12:11:27 +0530

    test update

--- test/var_parser_test.rb
+++ test/var_parser_test.rb
@@ -7,7 +7,7 @@ class VarParserTest < Minitest::Test
 
   def test_extract_instance_vars_works
 #    list = VarParser::extract_instance_vars('/home/dinesh/projects/pc/var_parser/test/sample.txt')
-    list = VarParser::extract_instance_vars('/home/dinesh/projects/pc/var_parser/test/sample.txt1')
+    list = VarParser::extract_instance_vars('/home/dinesh/projects/pc/var_parser/test/sample.txt')
     assert_equal 2,list.size
     assert_equal '@abc',list[0]
     assert_equal '@string',list[1]
